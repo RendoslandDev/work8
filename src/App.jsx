f46ba61147740f89137f6e9c62d35097297f3abe
RendoslandDev <agyapongrendosland53@gmail.com>
2024-12-01 22:54:05 -0800

    rendosland changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,17 @@ function App() {
 
   return (
     <>
-             <section className="bg-white p-4  sm:p-10 flex   justify-center mx-auto  ">
+             <section className="bg-white p-4  sm:p-10 border border-black-400   justify-center flex flex-col gap-6 xl:flex-row gap-10">
             <form className="  w-full lg:w-[47%] rounded-2xl p-3 sm:p-8   flex items-center justify-center">
                 <div className=" w-full">
                    
-                    <div className="flex flex-col xl:gap-6 xl:flex-row justify-between">
+                    <div className="flex flex-row gap-4 xl:gap-6 xl:flex-row justify-between">
                         <div className="flex flex-col w-full xl:w-1/2">
                             
                             <input
                                 type="text"
                                 name="user-name"
-                               className="rounded-lg my-2 w-full border border-gray-200  py-3 px-10 bg-gray-200 text-black rounded w-full rounded-lg my-2"
+                               className="rounded my-2 w-full border border-gray-200  py-3 px-5 bg-gray-200 text-black my-2"
                                 placeholder="Enter Name"
                             />
                         </div>
@@ -29,18 +29,18 @@ function App() {
                             <input
                                 type="text"
                                 name="user-name"
-                               className="rounded-lg my-2 w-full border border-gray-200  py-3 px-10 bg-gray-200 text-black rounded w-full rounded-lg my-2"
+                               className="rounded my-2 w-full border border-gray-200  py-3 px-5 bg-gray-200 text-black my-2"
                                 placeholder="Enter Email"
                             />
                         </div>
                     </div>
-                    <div className="flex flex-col xl:gap-6 xl:flex-row justify-between">
+                    <div className="flex flex-row gap-4 xl:gap-6 xl:flex-row justify-between">
                         <div className="flex flex-col w-full xl:w-1/2">
                          
                             <input
                                 type="text"
                                 name="user-name"
-                                className="rounded-lg my-2 w-full border border-gray-200  py-3 px-10 bg-gray-200 text-black rounded w-full rounded-lg my-2"
+                                className="rounded my-2 w-full border border-gray-200  py-3 px-5 bg-gray-200 text-black my-2"
                                 placeholder="From Subject"
                             />
                         </div>
@@ -50,7 +50,7 @@ function App() {
                             <input
                                 type="text"
                                 name="user-name"
-                             className="rounded-lg my-2 w-full border border-gray-200  py-3 px-10 bg-gray-200 text-black rounded w-full rounded-lg my-2"
+                             className="rounded my-2 w-full border border-gray-200  py-3 px-5 bg-gray-200 text-black my-2"
                                 placeholder="Enter Phone"
                             />
                         </div>
@@ -59,7 +59,7 @@ function App() {
                     <div className="flex flex-col  my-2">
                       
                         <textarea name="" id="" cols="30" rows="10" 
-                            className=" rounded-lg mb-5  bg-gray-200 w-ful pl-2 "
+                            className=" rounded mb-5  bg-gray-200 w-ful pl-2 "
                             placeholder='Your Message'
                         ></textarea>
                     </div>
@@ -69,21 +69,22 @@ function App() {
                     </div>
                 </div>
             </form>
-            <div className='hidden w-[47%]  h-screen lg:flex items-center'>
-                <div className='w-1/2   '>
-          <h1 className='font-bold '>For more enquiries?</h1>
-          <p className=''>you can reach out to us using</p>
-          <p>these dev's</p><br />
-          <ul role='list'>
+            <div className=' lg:flex  pt-10 xl:pt-40 '>
+                <div>
+          <h1 className='font-bold flex justify-center xl:justify-self-start '>For more enquiries?</h1>
+          <p className=' flex justify-center xl:items-end'>you can reach out to us using these </p>
+          <p className=' flex justify-center xl:justify-self-start'>these details</p>
+          <ul role='list' className='flex  flex-col justify-center  mt-5 '>
             <li class="group/item hover:bg-slate-100 ... flex mb-5">
-                <img src="images/imgphone.png" className='h-10 w-10 rounded' alt="" />
-            <div className='px-2'>
-             <a href="{person.url}">Phone</a>
-             <p>+233 678 874 255</p>
-            </div>
-           </li>
+         <img src="images/imgphone.png" className='h-10 w-10 rounded' alt="" />
+
+      <div className='px-2'>
+        <a href="{person.url}">Email</a>
+        <p>0558471039</p>
+      </div>
+      </li>
       <li class="group/item hover:bg-slate-100 ... flex mb-5">
-         <img src="images/imgweb.jpg" className='h-10 w-10 rounded' alt="" />
+         <img src="images/eml.png" className='h-10 w-10 rounded' alt="" />
 
       <div className='px-2'>
         <a href="{person.url}">Email</a>
