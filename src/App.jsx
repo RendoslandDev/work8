db6dc201b6acd0bad012a3d4a8f3a21c2a4a44be
RendoslandDev <agyapongrendosland53@gmail.com>
2024-12-02 21:17:53 -0800

    rendosland changes (icons changed)

--- src/App.jsx
+++ src/App.jsx
@@ -75,29 +75,28 @@ function App() {
           <p className=' flex justify-center xl:items-end'>you can reach out to us using these </p>
           <p className=' flex justify-center xl:justify-self-start'>these details</p>
           <ul role='list' className='flex  flex-col justify-center  mt-5 '>
-            <li class="group/item hover:bg-slate-100 ... flex mb-5">
-         <img src="images/imgphone.png" className='h-10 w-10 rounded' alt="" />
+            <li className="group/item hover:bg-slate-100 ... flex mb-5">
+         <i className="fa-solid fa-phone h-10 w-10 rounded border border-black-400 bg-black grid place-items-center text-white"></i>
 
       <div className='px-2'>
         <a href="{person.url}">Email</a>
         <p>0558471039</p>
       </div>
       </li>
-      <li class="group/item hover:bg-slate-100 ... flex mb-5">
-         <img src="images/eml.png" className='h-10 w-10 rounded' alt="" />
-
+      <li className="group/item hover:bg-slate-100 ... flex mb-5">
+      <i class="fa-solid fa-envelope-circle-check h-10 w-10 text-white bg-black rounded grid place-items-center"></i>
       <div className='px-2'>
         <a href="{person.url}">Email</a>
         <p>charlesowusu@gmail.com</p>
       </div>
       </li>
-      <li class="group/item hover:bg-slate-100 ... flex">
-      <img src="images/imgweb.jpg" className='h-10 w-10 rounded' alt="" />
-      <div className='px-2'>
+      <li className="group/item hover:bg-slate-100 ... flex ">
+     <i class="fa-solid fa-globe border border-black w-10 grid place-items-center rounded h-10 text-white bg-black"></i> 
+     <div className='px-2'>
         <a href="{person.url}">Website</a>
         <p>www.webdev.com.pvd</p>
       </div>
-      </li>
+      </li> 
           </ul>
            
 
@@ -113,4 +112,5 @@ function App() {
 
 export default App
 
+   
 
